cea34565ecea0c4311488c84f95f702af30d0895
Rabin Pun <rabinpun@Rabins-Mac-mini.local>
2025-07-13 01:00:33 +0545

    feature/detailsScreen: update the detailsscreen

--- IMDB/Screens/App/Details/DetailsScreen.swift
+++ IMDB/Screens/App/Details/DetailsScreen.swift
@@ -12,7 +12,45 @@ struct DetailsScreen: View {
     let movie: Movie
     
     var body: some View {
-        Text(movie.title ?? "No title")
+        ScrollView {
+               VStack(alignment: .leading, spacing: 16) {
+
+                   // Poster
+                   posterImage()
+                   
+                   // Title
+                   Text(movie.title ?? "")
+                       .font(.title)
+                       .fontWeight(.bold)
+
+                   // Release Date
+                   Text("Release Date: \(movie.releaseDate!.formattedDate)")
+                       .font(.subheadline)
+                       .foregroundColor(.secondary)
+
+                   // Overview
+                   Text(movie.overview ?? "")
+                       .font(.body)
+                       .multilineTextAlignment(.leading)
+
+                   Spacer()
+               }
+               .padding()
+           }
+           .navigationTitle("Movie Details")
+           .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    @ViewBuilder
+    func posterImage() -> some View {
+        Group {
+            if let posterPath = movie.posterImagePath, let url = URL(string: posterPath.originalImagePath) {
+                CachedAsyncImage(url: url)
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity)
+                    .cornerRadius(12)
+            }
+        }
     }
 }
 

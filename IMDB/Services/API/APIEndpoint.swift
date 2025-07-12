a58d3e53cd34863e59177d44055083a00fc5542a
Rabin Pun <rabinpun@Rabins-Mac-mini.local>
2025-07-12 15:12:11 +0545

    feature/home/apiIntegration: add apiendpoint

---
+++ IMDB/Services/API/APIEndpoint.swift
@@ -0,0 +1,29 @@
+//
+//  APIEndpoint.swift
+//  IMDB
+//
+//  Created by Rabin Pun on 12/07/2025.
+//
+
+import Foundation
+
+enum APIEndpoint {
+    
+    static let paginationSize = 20 // pagination size of the api response for list results. seems to be static for some reason.
+    
+    var baseURL: String { "https://api.themoviedb.org/3" }
+    case search
+
+    var urlString: String {
+        switch self {
+        case .search:
+            return "\(baseURL)/search/movie"
+        }
+    }
+    
+    var httpMethod: String {
+            switch self {
+            case .search: return "GET"
+        }
+    }
+}
